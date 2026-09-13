e1fb960d3e7e9b8ed767c60590647c71b74d7eaa
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2026-09-13 10:00:17 -0600

    Add SNN-native affective regulator

---
+++ spikeforge/affect/__init__.py
@@ -0,0 +1,5 @@
+"""SNN-native internal affect regulation primitives."""
+
+from spikeforge.affect.regulator import AffectiveOutput, AffectiveRegulator
+
+__all__ = ["AffectiveOutput", "AffectiveRegulator"]
